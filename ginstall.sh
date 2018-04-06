fd4e66abd8eeffa96878ed55596de421aca39b94
tdoan5 <33384826+tdoan5@users.noreply.github.com>
2018-04-06 00:43:04 -0400

    Update links

--- ginstall.sh
+++ ginstall.sh
@@ -13,10 +13,10 @@ pip3 install --upgrade flask
 # add -N so that wget do not duplicate copied files when reset 
 # copy necessary files
 sudo wget -N --directory-prefix=$path/templates/ \
-https://raw.githubusercontent.com/tdoan5/gcloud/port_5000/templates/index.html
+https://raw.githubusercontent.com/tdoan5/gcloud/master/templates/index.html
 
 sudo wget -N --directory-prefix=$path/ \
-https://raw.githubusercontent.com/tdoan5/gcloud/port_5000/todolist.db
+https://raw.githubusercontent.com/tdoan5/gcloud/master/todolist.db
 
 sudo wget -N --directory-prefix=$path/ \
 https://raw.githubusercontent.com/tdoan5/gcloud/port_5000/todolist.py
